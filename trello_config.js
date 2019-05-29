09dbf56273f110f85f90aa1bf6909d801d65d96c
lesdman400 <lmercier@geiger.com>
2019-05-29 10:30:23 -0400

    tweaks

--- trello_config.js
+++ trello_config.js
@@ -3,3 +3,56 @@ var config = {
 	"token":""  //Enter Private Token
 	
 }
+
+var departments = {
+	"depCrestline":"",
+	"depFarmersAlmanac": "",
+	"depGeiger":"CorporateProgramsProgramManagersGroup1,CorporateProgramsProgramManagersGroup2,CorporateProgramsProgramManagersGroup3,CorporateProgramsProgramManagersPSG,CorporateProgramsWeb&GraphicsGroup1,CorporateProgramsWeb&GraphicsGroup2,CorporateProgramsCustomerAdvocates,AssistantProgramManagers&AdminAssistant,CorporateProgramsInventory&Pricing",
+	"depSupport":"",
+}
+
+var teams = {
+	// TEST Team
+	// "CPWEB":"bobdagget,jeffpatterson,pedmands,lesliemercier,preston409,johnpaine5,lesliemercier,johnathanplummer,dawnlaprell,mikefrench,mikeplourde,scottstowe,jallen19,alangroudle,tylerturcotte,jessicalevy10,kevinmcgrory,sthompsongeiger1",
+	"Graphics": "",
+	"Marketing/Brand/Data": "",
+	"Sales": "",
+	"SalesSupport": "",
+	"Farmers'Almanac": "",
+	"CorporateProgramsProgramManagersGroup1": "charlenedion,christamatuszewski,kimberlyblais,marcylavallee,kevinboilard,ryanmockler,sarahcouturier",
+	"CorporateProgramsProgramManagersGroup2": "jonisatterthwaite,kaitlynnegibson1,rachellepower,kmillercraft,scottlaberge,stelladuclos",
+	"CorporateProgramsProgramManagersGroup3": "jamesayotte3,justinfortier5,kellysutton3,nikkihamlin,rkilgore,theajones1",
+	"CorporateProgramsProgramManagersPSG": "crystalparadis,erinhutchinson,bshuster1,julianarua,juliestinson1,jvickers1,kevinboilard,priscillakistner,jenruler1,ambryant,stephaniehobbs,wendythorne,traceydespres,reneeperdicaro1",
+	"CorporateProgramsWeb&GraphicsGroup1": "dawnlaprell,mikefrench,sthompsongeiger1,mikeplourde,johnpaine5,preston409,scottstowe",
+	"CorporateProgramsWeb&GraphicsGroup2": "alangroudle,jallen19,jessicalevy10,johnathanplummer,lesliemercier,tylerturcotte,kevinmcgrory",
+	"CorporateProgramsCustomerAdvocates": "amandapaul,andreasands2,hcote2,heidi96961679,madisonsawyer,pamelamorris4",
+	"AssistantProgramManagers&AdminAssistant": "amyrioux4,brandivachon,jrbourgoin,joshnickerson3,rubyphillips,scottsmith209",
+	"CorporateProgramsInventory&Pricing": "bevhemond,kaseyplourde,katherineparlove,katiedoucette1",
+	"RiskManagement": "",
+	"DC": "",
+	"Finance": "",
+	"Marketing": "",
+	"TotalCareA/P&FreightPayables": "",
+	"TotalCareA/R": "",
+	"TotalCareBillingGroup": "",
+	"TotalCareClaims": "",
+	"TotalCareCRSThinkTankElite": "",
+	"TotalCareCRSTurnItUp": "",
+	"TotalCareCRSMightyGreenDots": "",
+	"TotalCareCRSConceptCrew": "",
+	"TotalCareCRSEastFlorida": "",
+	"TotalCareCRSDefendersofTrello": "",
+	"TotalCareCRSMellowTrello": "",
+	"TotalCareCRSWest": "",
+	"TotalCareCRSLewiston": "",
+	"TotalCareCRSServiceNow": "",
+	"TotalCareOrderExpediting": "",
+	"TotalCareOrderVerifier": "",
+	"CorporateFinance": "",
+	"ExMgmt&AdminServices": "",
+	"VendorRelations": "",
+	"HR/OD/OS": "",
+	"ITEcommerce": "",
+	"ITHelpdesk/WebApps/NOC": "",
+	"ProductData": ""
+};
